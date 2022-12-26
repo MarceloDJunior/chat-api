5bcb80e715cb85dd27437f17141de6e58e10dcdb
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2022-12-26 00:15:47 -0300

    import user service in message service and validate if from and to user exists

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from 'src/users/users.module';
 import { Message } from './entities/message.entity';
 import { MessagesController } from './messages.controller';
 import { MessagesService } from './messages.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Message])],
+  imports: [TypeOrmModule.forFeature([Message]), UsersModule],
   controllers: [MessagesController],
   providers: [MessagesService],
 })
--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,13 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { In, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Message } from './entities/message.entity';
 import { SendMessageDto } from './dtos/send-message.dto';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class MessagesService {
   constructor(
     @InjectRepository(Message) private messagesRepository: Repository<Message>,
+    private readonly usersService: UsersService,
   ) {}
 
   async getRecentMessages(user1: number, user2: number): Promise<Message[]> {
@@ -32,6 +34,17 @@ export class MessagesService {
   }
 
   async sendMessage({ fromId, toId, text }: SendMessageDto): Promise<void> {
+    const fromUser = await this.usersService.findOne(fromId);
+
+    if (!fromUser) {
+      throw new NotFoundException('from user not found');
+    }
+
+    const toUser = await this.usersService.findOne(toId);
+    if (!toUser) {
+      throw new NotFoundException('to user not found');
+    }
+
     await this.messagesRepository.insert({
       fromId,
       toId,
