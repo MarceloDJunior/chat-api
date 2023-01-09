a5f49431b56a82f010b1e06c999999b27a05d45e
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2023-01-08 23:22:22 -0300

    move helpers to common folder

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, Matches, MinLength } from 'class-validator';
-import { PASSWORD_REGEX } from 'src/helpers/regex';
+import { PASSWORD_REGEX } from 'src/common/helpers/regex';
 
 export class CreateUserDto {
   @ApiProperty()
