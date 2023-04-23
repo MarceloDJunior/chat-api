f00cb0bce541123aef3f604b69659dc36abcbdf7
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2023-04-22 21:52:20 -0300

    add client cloudfront url to allowed origins

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,11 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, documentoConfig);
   SwaggerModule.setup('api', app, document);
 
-  app.enableCors();
+  app.enableCors({
+    origin: ['https://dru4mwnwwwwon.cloudfront.net'],
+    methods: ['GET', 'POST'],
+    credentials: true,
+  });
 
   const hostname = config.isDev ? '127.0.0.1' : '0.0.0.0';
   await app.listen(3000, hostname);
